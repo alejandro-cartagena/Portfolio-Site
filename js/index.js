5fb325389533a0275900b8a16e68117b6f9264b4
Alejandro <alejandrocartagena68@gmail.com>
2024-01-24 12:05:58 -0500

    Added scroll effect on header and fixed the spacing on some elements

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const header = document.querySelector("header")
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -9,4 +10,14 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
+})
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        header.classList.add("change")
+    }
+    else {
+        header.classList.remove("change")
+    }
+    
 })
