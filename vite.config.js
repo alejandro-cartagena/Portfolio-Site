ccd8b6388d03bcaee2c13c795b6a6c03778cd651
Alejandro <alejandrocartagena68@gmail.com>
2024-01-24 14:00:45 -0500

    Made changes to the vite.config.js file to resolve deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
 import {defineConfig} from 'vite'
+import { resolve } from 'path';
 
 
 export default defineConfig({
-	plugins: []	
+	plugins: [],
+	build: { 
+		rollupOptions: { 
+			input: { 
+				main: resolve(__dirname, 'index.html'), 
+				portfolio_item_1: resolve(__dirname, 'portfolio-item-1.html'), 
+				portfolio_item_2: resolve(__dirname, 'portfolio-item-2.html'),
+				portfolio_item_3: resolve(__dirname, 'portfolio-item-3.html'),
+				portfolio_item_4: resolve(__dirname, 'portfolio-item-4.html'),
+				portfolio_item_5: resolve(__dirname, 'portfolio-item-5.html'),
+				portfolio_item_6: resolve(__dirname, 'portfolio-item-6.html')
+			 }, 
+			},
+		 }, 
 })
+
+// export default defineConfig(
+// 	{ 
+// 		build: { 
+// 			rollupOptions: { 
+// 				input: { 
+// 					main: resolve(__dirname, 'index.html'), contact: resolve(__dirname, 'contact.html'), about: resolve(__dirname, 'about.html'),
+// 				 }, 
+// 				},
+// 			 }, 
+// 			});
